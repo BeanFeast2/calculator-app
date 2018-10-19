17588bca8a3d3cac9e0ccff85e0fa78b5c67a7fc
Vince Carollo <vincecarollo@gmail.com>
2018-10-18 21:14:56 -0600

    started adding calculator functionality

--- script.js
+++ script.js
@@ -31,4 +31,51 @@ function operate(operator, a, b){
   }
 }
 
-console.log(operate('/', 1, 2))
+
+let buttons = document.querySelectorAll('button');
+let displayNum = document.getElementById('nums');
+let runningTotal = '';
+let firstNum = 0;
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    let buttonContent = event.target.innerHTML;
+    let operator = '';
+    //if user clicks clear
+    if(buttonContent === 'Clear'){
+      displayNum.innerHTML = '';
+      runningTotal = '';
+      //if operator is clicked
+    }else if(buttonContent === '/' || buttonContent === 'x' || buttonContent === '-' || buttonContent === '+' || buttonContent === '=' ) {
+      if(firstNum === 0) {
+        firstNum = runningTotal;
+        runningTotal = '';
+      }
+      switch(buttonContent) {
+        case '/':
+          operator = '/';
+          displayNum.innerHTML = '';
+          break;
+        case 'x':
+          operator = '*';
+          displayNum.innerHTML = '';
+          break;
+        case '-':
+          operator = '-';
+          displayNum.innerHTML = '';
+          break;
+        case '+':
+          operator = '+';
+          displayNum.innerHTML = '';
+          break;
+        case '=':
+          displayNum.innerHTML = '';
+
+          break;
+      }
+    }else{
+      displayNum.innerHTML += buttonContent;
+      runningTotal += buttonContent;
+      console.log(runningTotal)
+    }
+  })
+})
